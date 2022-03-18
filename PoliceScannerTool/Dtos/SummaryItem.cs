93bef2a0ae9b322a517fc0af51e363f6950c56d1
JoshYoung-Cannon <jyoungcannon@gmail.com>
2022-03-18 15:44:09 +0000

    Get-All-Crimes Services

---
+++ PoliceScannerTool/Dtos/SummaryItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoliceScannerTool.Dtos
+{
+    public class SummaryItem
+    {
+        public SummaryItem(string crime)
+        {
+            Crime = crime;
+            Count = 0;
+        }
+
+        public SummaryItem(string crime, int count)
+        {
+            Crime = crime;
+            Count = count;
+        }
+
+        public string Crime { get; set; }
+        public int Count { get; set; }
+    }
+}
