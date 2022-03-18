fa3a94aebb47040607ab7ffb2985f19d675d5318
JoshYoung-Cannon <jyoungcannon@gmail.com>
2022-03-18 14:16:29 +0000

    Get-Latest-Date Services & Tests

---
+++ API-TestCases/SetupServicesUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PoliceScannerTool.Dtos;
+using PoliceScannerTool.Services;
+
+namespace API_TestCases
+{
+    [TestClass]
+    public class SetupServicesUnitTest
+    {
+        [TestMethod]
+        public void ConvertDateDtoToFormTest()
+        {
+            SetupServices setupTool = new SetupServices();
+            FormDate goal = new FormDate(2022, 03);
+            DateDto dateDto = new DateDto();
+            dateDto.Date = "2022-03-18";
+
+            FormDate result = setupTool.ConvertDateDtoToForm(dateDto);
+
+            Assert.AreEqual(goal.Year, result.Year);
+            Assert.AreEqual(goal.Month, result.Month);
+        }
+        [TestMethod]
+        public void GetLatestUpdate()
+        {
+            SetupServices setupTool = new SetupServices();
+            var returnVal = setupTool.GetLatestUpdate();
+            Assert.IsInstanceOfType(returnVal, typeof(FormDate));
+        }
+    }
+}
--- PoliceScannerTool/Services/SetupServices.cs
+++ PoliceScannerTool/Services/SetupServices.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace PoliceScannerTool.Services
 {
-    class SetupServices
+    public class SetupServices
     {
         public FormDate ConvertDateDtoToForm(DateDto inputDto)
         {
