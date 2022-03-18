68671bd04e1d179d22017c7f172b11e6ddbdcf81
JoshYoung-Cannon <jyoungcannon@gmail.com>
2022-03-18 14:04:33 +0000

    Limiting User Input For Date

---
+++ PoliceScannerTool/Services/SetupServices.cs
@@ -0,0 +1,29 @@
+﻿using PoliceScannerTool.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoliceScannerTool.Services
+{
+    class SetupServices
+    {
+        public FormDate ConvertDateDtoToForm(DateDto inputDto)
+        {
+            string[] components = inputDto.Date.Split('-');
+            int year = Convert.ToInt32(components[0]);
+            int month = Convert.ToInt32(components[1]);
+
+            return new FormDate(year, month);
+        }
+
+        public FormDate GetLatestUpdate()
+        {
+            API apiTool = new API();
+            DateDto apiResponse = apiTool.GetLatestUpdate();
+
+            return ConvertDateDtoToForm(apiResponse);
+        }
+    }
+}
