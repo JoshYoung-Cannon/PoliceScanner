954e6d864fdefec46a7723fda7f22c9b52ea0605
JoshYoung-Cannon <jyoungcannon@gmail.com>
2022-03-18 12:46:28 +0000

    Form Skeleton Code

---
+++ PoliceScannerTool/Dtos/FormDate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoliceScannerTool.Dtos
+{
+    class FormDate
+    {
+        public FormDate(int year, int month)
+        {
+            Year = year;
+            Month = month;
+        }
+        public int Year { get; set; }
+        public int Month { get; set; }
+
+    }
+}
--- PoliceScannerTool/Form1.cs
+++ PoliceScannerTool/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PoliceScannerTool.Dtos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,26 @@ namespace PoliceScannerTool
 {
     public partial class Form1 : Form
     {
+        private FormDate LatestDate { get; set; }
+        private FormDate EarliestDate { get; set; }
+
         public Form1()
         {
             InitializeComponent();
+            // get date limits
+            // set date limits
+        }
+
+        private void YearInput_ValueChanged(object sender, EventArgs e)
+        {
+            // Update valid month selections
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            // Get and format inputs
+            // Send API request
+            // Display result
         }
     }
 }
