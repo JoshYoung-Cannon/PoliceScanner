2f60f95a9c5219daee1870ef82507ec791da4f4e
JoshYoung-Cannon <jyoungcannon@gmail.com>
2022-03-18 14:23:40 +0000

    CrimeDto created

---
+++ PoliceScannerTool/Dtos/LocationDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoliceScannerTool.Dtos
+{
+    public class LocationDto
+    {
+        public string latitude { get; set; }
+        public StreetDto street { get; set; }
+        public string longitude { get; set; }
+    }
+}
---
+++ PoliceScannerTool/Dtos/OutcomeStatusDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoliceScannerTool.Dtos
+{
+    public class OutcomeStatusDto
+    {
+        public string category { get; set; }
+        public string date { get; set; }
+    }
+}
